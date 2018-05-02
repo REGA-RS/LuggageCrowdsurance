b80747db1e0af98f257f96c40933c9d67ab22235
Sergei Sevriugin <sevriugin@gmail.com>
2018-05-02 21:25:24 +0200

    adding new react comp. to fix data view

---
+++ src/layouts/home/BalanceData.js
@@ -0,0 +1,107 @@
+import { drizzleConnect } from 'drizzle-react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+/*
+ * Create component.
+ */
+
+class BalanceData extends Component {
+  constructor(props, context) {
+    super(props);
+
+    this.precisionRound = this.precisionRound.bind(this);
+
+    this.contracts = context.drizzle.contracts
+
+    // Get the contract ABI
+    const abi = this.contracts[this.props.contract].abi;
+
+    // Fetch initial value from chain and return cache key for reactive updates.
+    var methodArgs = [this.props.accounts[this.props.accountIndex]];
+
+    if (this.props.viewOnly) {
+        methodArgs = [];
+    }
+    
+    this.dataKey = this.contracts[this.props.contract].methods[this.props.method].cacheCall(...methodArgs);
+
+    // Iterate over abi for correct function.
+    for (var i = 0; i < abi.length; i++) {
+      if (abi[i].name === this.props.method) {
+          this.fnABI = abi[i];
+
+          break;
+      }
+    }
+  }
+
+  precisionRound(number, precision) {
+    var factor = Math.pow(10, precision)
+    return Math.round(number * factor) / factor
+  }
+
+  render() {
+    // No accounts found.
+    if(Object.keys(this.props.accounts).length === 0 || !this.props.contracts[this.props.contract].initialized) {
+      return (
+        <span>Initializing...</span>
+      )
+    }
+
+    // If the cache key we received earlier isn't in the store yet; the initial value is still being fetched.
+    if(!(this.dataKey in this.props.contracts[this.props.contract][this.props.method])) {
+        return (
+          <span>Fetching...</span>
+        )
+    }
+
+    var pendingSpinner = this.props.contracts[this.props.contract].synced ? '' : ' 🔄'
+
+    // Optionally hide loading spinner (EX: ERC20 token symbol).
+    if (this.props.hideIndicator) {
+      pendingSpinner = ''
+    }
+
+    var displayData = this.props.contracts[this.props.contract][this.props.method][this.dataKey].value
+
+    // Get account address and balance.
+    var balance = displayData;
+    // const units = this.props.units ? this.props.units.charAt(0).toUpperCase() + this.props.units.slice(1) : 'Wei'
+
+    if (this.props.correction) {
+        balance = balance.slice(0,balance.length - this.props.correction)
+    }
+
+    // Convert to given units.
+    if (this.props.units) {
+      balance = this.context.drizzle.web3.utils.fromWei(balance, this.props.units)
+    }
+
+    // Adjust to given precision.
+    if (this.props.precision) {
+      balance = this.precisionRound(balance, this.props.precision)
+    }
+
+    return(
+        <span>{balance} {pendingSpinner}</span>
+    )
+  }
+}
+
+BalanceData.contextTypes = {
+  drizzle: PropTypes.object
+}
+
+/*
+ * Export connected component.
+ */
+
+const mapStateToProps = state => {
+  return {
+    accounts: state.accounts,
+    contracts: state.contracts    
+  }
+}
+
+export default drizzleConnect(BalanceData, mapStateToProps)
--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { AccountData, ContractData, ContractForm } from 'drizzle-react-components'
 import rega from '../../rega.png'
+import BalanceData  from './BalanceData.js'
 
 class Home extends Component {
   render() {
@@ -28,10 +29,9 @@ class Home extends Component {
             <p><strong>RST Address</strong>: <ContractData contract="LCSToken" method="RST" /></p>
             <p><strong>LCS Address</strong>: <ContractData contract="ERC20Adapter" method="controller" /></p>
             <p><strong>LCS Owner</strong>: <ContractData contract="LCSToken" method="owner" /></p>
-            <p><strong>RST Join Amount</strong>: <ContractData contract="LCSToken" method="joinAmountRST" /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
-            <p><strong>ETH Only</strong>: <ContractData contract="LCSToken" method="ETHOnly" /></p>
+            <p><strong>RST Join Amount</strong>: <BalanceData contract="LCSToken" method="joinAmountRST" accountIndex="0" units="nano" correction="1" precision="3" viewOnly /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
+            <p><strong>ERC20 Balance</strong>: <BalanceData contract="ERC20Adapter" method="balanceOf" accountIndex="0" units="ether" precision="3" /> Ether </p>
             <p><strong>LCS Balance</strong>: <ContractData contract="LCSToken" method="balanceOf" methodArgs={[this.props.accounts[0]]} /> <ContractData contract="LCSToken" method="symbol" hideIndicator /> </p>
-            <p><strong>ERC20 Balance</strong>: <ContractData contract="ERC20Adapter" method="balanceOf" methodArgs={[this.props.accounts[0]]} /> <ContractData contract="ERC20Adapter" method="symbol" hideIndicator /> </p>
             <h3>Score new member [owner only]</h3>
             <ContractForm contract="LCSToken" method="scoring" labels={['Member Address', 'Member Score', 'Join Amount']}/>
 
@@ -41,8 +41,8 @@ class Home extends Component {
           <div className="pure-u-1-1">
             <h2>RSTToken</h2>
             <h3>Transfer some RST Tokens to the new member.</h3>
-            <p><strong>Total Supply</strong>: <ContractData contract="RSTToken" method="totalSupply" /> <ContractData contract="RSTToken" method="symbol" hideIndicator /></p>
-            <p><strong>My Balance</strong>: <ContractData contract="RSTToken" method="balanceOf" methodArgs={[this.props.accounts[0]]} /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
+            <p><strong>Total Supply</strong>: <BalanceData contract="RSTToken" method="totalSupply" accountIndex="0" units="nano" correction="1" precision="3" viewOnly /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
+            <p><strong>My Balance</strong>: <BalanceData contract="RSTToken" method="balanceOf" accountIndex="0" units="nano" precision="3" correction="1" /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
             <h3>Token transfer</h3>
             <ContractForm contract="RSTToken" method="transfer" labels={['To Address', 'Amount to Transfer']} />
             
@@ -52,8 +52,8 @@ class Home extends Component {
           <div className="pure-u-1-1">
             <h2>RSTToken</h2>
             <h3>Now the new member need to approve token transfer.</h3>
-            <p><strong>Total Supply</strong>: <ContractData contract="RSTToken" method="totalSupply" /> <ContractData contract="RSTToken" method="symbol" hideIndicator /></p>
-            <p><strong>My Balance</strong>: <ContractData contract="RSTToken" method="balanceOf" methodArgs={[this.props.accounts[0]]} /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
+            <p><strong>Total Supply</strong>: <BalanceData contract="RSTToken" method="totalSupply" accountIndex="0" units="nano" correction="1" precision="3" viewOnly /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
+            <p><strong>My Balance</strong>: <BalanceData contract="RSTToken" method="balanceOf" accountIndex="0" units="nano" precision="3" correction="1" /> <ContractData contract="RSTToken" method="symbol" hideIndicator /> </p>
             <p><strong>Allowance</strong>: <ContractData contract="RSTToken" method="allowance" methodArgs={[this.props.accounts[0],"0x9A343c4BD1676736872Ba4e531555b7924c72458"]} /> <ContractData contract="RSTToken" method="symbol" hideIndicator /></p>
             <h3>Approve token transfer</h3>
             <ContractForm contract="RSTToken" method="approve" labels={['To Address', 'Amount to Approve']} />
