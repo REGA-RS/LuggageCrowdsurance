fcefebc6f570aa895356e468c13b4cc5f6038038
Sergei Sevriugin <sevriugin@gmail.com>
2018-05-28 17:38:48 +0300

    Ropsten build

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "plate mix any adult deal sand brand about window will casual grit";
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -5,7 +8,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/BymPe6F1XQoQ3ZwlEzme")
+      },
+      network_id: 3
+    }   
   },
   solc: {
     optimizer: {
